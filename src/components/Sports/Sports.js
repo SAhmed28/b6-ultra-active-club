fa810c44ac30a111702117fb4e5fa7441cbb0354
SAhmed28 <sanjida1@ualberta.ca>
2022-09-29 04:12:56 -0600

    qa added

--- src/components/Sports/Sports.js
+++ src/components/Sports/Sports.js
@@ -42,7 +42,24 @@ const Sports = () => {
                         }
                     </div>
 
-                    
+                    <div className="qa row">
+                        <div class="card">
+                            <div class="card-body">
+                                <div className="card-title">Q1: How does React work?</div>
+                                <div className="card-text">Ans. React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process. It uses Babel compiler to compile JSX to pure JS code.</div>
+                            </div>
+                            <div class="card-body">
+                                <div className="card-title">Q2: Difference between props and state?</div>
+                                <div className="card-text">Ans. Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. Props are readable but we can't modify it from anywhere other than the place it was sent.</div>
+                            </div>
+                            <div class="card-body">
+                                <div className="card-title">Q1: Use of useEffect</div>
+                                <div className="card-text">Ans. By using this Hook, you tell React that your component needs to do something after render. React will remember the function you passed (we'll refer to it as our “effect”), and call it later after performing the DOM updates.</div>
+                            </div>
+                        </div>
+
+                        
+                    </div>
                 </div>
                 
                 <div className="col-lg-3 right">
