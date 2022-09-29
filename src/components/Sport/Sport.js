ca4fa1f3c896bc5c6b95f5e28c309d6531cac58f
SAhmed28 <sanjida1@ualberta.ca>
2022-09-29 02:30:56 -0600

    sports button design fixed

--- src/components/Sport/Sport.js
+++ src/components/Sport/Sport.js
@@ -11,9 +11,8 @@ const Sport = (props) => {
                 <div class="card-body">
                     <h5 class="card-title">{title}</h5>
                     <p class="card-text">{desc}</p>
-                    <p class="card-text">For Age: 20-25</p>
                     <p class="card-text time">Time Required: {time}min</p>
-                    <button onClick={() => props.handleAddToList(time)} className="btn btn-primary">Add to list</button>
+                    <button onClick={() => props.handleAddToList(time)} className="btn-add">Add to list</button>
                 </div>
             </div>
         </div>
